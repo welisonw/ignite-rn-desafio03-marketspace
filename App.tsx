c9ffba22de57a486e0c6c8a0275e9fb0a10bfc46
Welison Wessling <wwelisonw@gmail.com>
2024-01-23 17:06:26 -0300

    feat: adjusted font settings in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+	useFonts,
+	Karla_400Regular,
+	Karla_700Bold,
+} from '@expo-google-fonts/karla';
+import { NativeBaseProvider } from 'native-base';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	let [fontsLoaded] = useFonts({
+		Karla_400Regular,
+		Karla_700Bold,
+	});
+
+	return (
+		<NativeBaseProvider>
+        <StatusBar style='auto' />
+        {
+          fontsLoaded ? (
+            <Text>Open up App.tsx to start working on your app!</Text>
+          ) : (
+            <View />
+          )
+        }
+    </NativeBaseProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
