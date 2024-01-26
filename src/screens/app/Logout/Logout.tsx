128b809fe2f3c9c9be260c30afc96a91c8038fb0
Welison Wessling <wwelisonw@gmail.com>
2024-01-26 11:54:47 -0300

    feat: created basic structure of bottom tab navigation screens (Home, MyAdvertisements and Logout)

---
+++ src/screens/app/Logout/Logout.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from 'native-base';
+
+export const Logout = () => {
+	return (
+		<Center flex={1}>
+			<Text>Saindo...</Text>
+		</Center>
+	);
+};
---
+++ src/screens/app/MyAdvertisements/MyAdvertisements.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "native-base";
+
+export const MyAdvertisements = () => {
+  return (
+    <Center flex={1}>
+      <Text>Meus anúncios</Text>
+    </Center>
+  );
+};
