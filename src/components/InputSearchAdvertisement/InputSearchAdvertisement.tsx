f16a2f751017dbe99941fbc9d4d2822c18a0c786
Welison Wessling <wwelisonw@gmail.com>
2024-01-27 17:57:33 -0300

    feat: created InputSearchAdvertisements component

---
+++ src/components/InputSearchAdvertisement/InputSearchAdvertisement.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import { LogBox } from 'react-native';
+import {
+	Divider,
+	HStack,
+	Pressable,
+	Spinner,
+	Text,
+	VStack,
+	useTheme,
+} from 'native-base';
+
+import { MagnifyingGlass, Sliders } from 'phosphor-react-native';
+
+import { Input } from '@components/Input/Input';
+
+LogBox.ignoreLogs(['NativeBase']);
+
+export const InputSearchAdvertisement = () => {
+	const [isSearching, setIsSearching] = useState(false);
+
+	const { colors } = useTheme();
+
+	function handleSearch() {
+		console.log('Pesquisar anúncio');
+	}
+
+	function handleOpenModal() {
+		console.log('Modal aberto');
+	}
+
+	return (
+		<VStack marginBottom={6}>
+			<Text fontFamily='body' fontSize='sm' color='gray.500' marginBottom={3}>
+				Seus produtos anunciados para venda
+			</Text>
+
+			<Input
+				placeholder='Buscar anúncio'
+				InputRightElement={
+					<HStack space={3} padding={['12.5px']}>
+						{isSearching ? (
+							<Spinner size='sm' color={colors.gray[600]} />
+						) : (
+							<Pressable
+								onPress={handleSearch}
+								_pressed={{
+									opacity: 0.5,
+								}}
+							>
+								<MagnifyingGlass
+									size={20}
+									weight='bold'
+									color={colors.gray[600]}
+								/>
+							</Pressable>
+						)}
+
+						<Divider
+							orientation='vertical'
+							height={18}
+							backgroundColor='gray.400'
+						/>
+
+						<Pressable
+							onPress={handleOpenModal}
+							_pressed={{
+								opacity: 0.5,
+							}}
+						>
+							<Sliders size={20} weight='bold' color={colors.gray[600]} />
+						</Pressable>
+					</HStack>
+				}
+			/>
+		</VStack>
+	);
+};
--- src/screens/app/Home/Home.tsx
+++ src/screens/app/Home/Home.tsx
@@ -1,10 +1,17 @@
-import { HomeHeader } from "@components/HomeHeader/HomeHeader";
-import { Center, Text, VStack } from "native-base";
+import { VStack } from 'native-base';
+
+import { HomeHeader } from '@components/HomeHeader/HomeHeader';
+import { CardInfoMyAdvertisements } from '@components/CardInfoMyAdvertisements/CardInfoMyAdvertisements';
+import { InputSearchAdvertisement } from '@components/InputSearchAdvertisement/InputSearchAdvertisement';
 
 export const Home = () => {
-  return (
-    <VStack flex={1} paddingTop={16} px={6} backgroundColor='gray.200'>
-      <HomeHeader />
-    </VStack>
-  );
+	return (
+		<VStack flex={1} paddingTop={16} px={6} backgroundColor='gray.200'>
+			<HomeHeader />
+
+			<CardInfoMyAdvertisements />
+
+			<InputSearchAdvertisement />
+		</VStack>
+	);
 };
