3296b85db0a8d55cbc5286cf5c678e24905c7d6a
Welison Wessling <wwelisonw@gmail.com>
2024-01-27 17:56:28 -0300

    feat: created CardInfoMyAdvertisements component

---
+++ src/components/CardInfoMyAdvertisements/CardInfoMyAdvertisements.tsx
@@ -0,0 +1,63 @@
+import {
+	HStack,
+	Heading,
+	Pressable,
+	Text,
+	VStack,
+	useTheme,
+} from 'native-base';
+
+import { ArrowRight, Tag } from 'phosphor-react-native';
+
+function handleGoMyAdvertisements() {
+	console.log('Ir para screen meus anúncios');
+}
+
+export const CardInfoMyAdvertisements = () => {
+	const { colors } = useTheme();
+
+	return (
+		<VStack marginBottom={8}>
+			<Text fontFamily='body' fontSize='sm' color='gray.500' marginBottom={3}>
+				Seus produtos anunciados para venda
+			</Text>
+
+			<HStack
+				alignItems='center'
+				py={3}
+				paddingLeft={4}
+				paddingRight={5}
+				backgroundColor='#6479c71a'
+				borderRadius='md'
+				space={4}
+			>
+				<Tag size={22} color={colors.blue[500]} />
+
+				<VStack flex={1}>
+					<Heading fontFamily='heading' fontSize='lg' color='gray.600'>
+						4
+					</Heading>
+
+					<Text fontFamily='body' fontSize='xs' color='gray.600'>
+						anúncios ativos
+					</Text>
+				</VStack>
+
+				<Pressable
+					onPress={handleGoMyAdvertisements}
+					_pressed={{
+						opacity: 0.7,
+					}}
+				>
+					<HStack alignItems='center' space={2}>
+						<Heading fontFamily='heading' fontSize='xs' color='blue.500'>
+							Meus anúncios
+						</Heading>
+
+						<ArrowRight size={16} color={colors.blue[500]} />
+					</HStack>
+				</Pressable>
+			</HStack>
+		</VStack>
+	);
+};
