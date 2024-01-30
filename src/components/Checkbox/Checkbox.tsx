4d5f893bf802b3fc0d5584cba69f86f7dd77503a
Welison Wessling <wwelisonw@gmail.com>
2024-01-30 17:55:36 -0300

    feat: created Checkbox component

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,43 @@
+import { Checkbox as CheckboxNativeBase, Text, useTheme } from 'native-base';
+
+interface CheckboxProps {
+	value: string;
+	label: string;
+	isChecked: boolean;
+	onChange: (isChecked: boolean) => void;
+	errorMessage?: string | null;
+}
+
+export const Checkbox = ({
+	value,
+	label,
+	isChecked,
+	onChange,
+}: CheckboxProps) => {
+	const { colors } = useTheme();
+
+	return (
+		<CheckboxNativeBase
+			value={value}
+			alignItems='center'
+			marginBottom={2}
+			borderColor={colors.gray[400]}
+			defaultIsChecked
+			_checked={{
+				background: colors.blue[300],
+				borderColor: colors.blue[300],
+			}}
+			onChange={() => onChange(!isChecked)}
+		>
+			<Text
+				fontFamily='body'
+				fontSize='md'
+				color='gray.600'
+				marginLeft={2}
+				textTransform='capitalize'
+			>
+				{label}
+			</Text>
+		</CheckboxNativeBase>
+	);
+};
