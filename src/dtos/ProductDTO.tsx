2fea641eccf4eae64c8a77d1f070ed876c7e3abf
Welison Wessling <wwelisonw@gmail.com>
2024-01-31 17:38:29 -0300

    feat: created ProductDTO

---
+++ src/dtos/ProductDTO.tsx
@@ -0,0 +1,17 @@
+import { PaymentMethodDTO } from './PaymentMethodsDTO';
+import { UserDTO } from './UserDTO';
+import { ProductImagesDTO } from './ProductImagesDTO';
+
+export interface ProductDTO {
+	id: string;
+	name: string;
+	description: string;
+	is_new: boolean;
+	price: number;
+	accept_trade: boolean;
+	payment_methods: PaymentMethodDTO[];
+	user_id: string;
+	user: UserDTO;
+	is_active: boolean;
+	product_images: ProductImagesDTO[];
+}
