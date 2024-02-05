e79168b2ca6832116e167db32cb725584d496aa3
Welison Wessling <wwelisonw@gmail.com>
2024-02-05 14:01:15 -0300

    feat: added variantBgColor parameter with colorMap to ProductConditionTag component

--- src/components/ProductConditionTag/ProductConditionTag.tsx
+++ src/components/ProductConditionTag/ProductConditionTag.tsx
@@ -1,18 +1,43 @@
 import { Text, View } from 'native-base';
 
+const colorMap = {
+	blueLight: 'blue.300',
+	blueDark: 'blue.500',
+	grayLight: 'gray.300',
+	grayDark: 'gray.600',
+};
+
 interface ProductConditionTagProps {
 	isNew: boolean;
+	variantBgColor: keyof typeof colorMap;
 }
 
-export const ProductConditionTag = ({ isNew }: ProductConditionTagProps) => {
+export const ProductConditionTag = ({
+	isNew,
+	variantBgColor,
+}: ProductConditionTagProps) => {
 	return (
 		<View
 			px={2}
 			py={'2px'}
 			rounded='full'
-			backgroundColor={isNew ? 'blue.500' : 'gray.600'}
+			backgroundColor={
+				isNew && variantBgColor === 'blueDark'
+					? colorMap[variantBgColor]
+					: isNew && variantBgColor === 'blueLight'
+					? colorMap[variantBgColor]
+					: !isNew && variantBgColor === 'grayDark'
+					? colorMap[variantBgColor]
+					: !isNew && variantBgColor === 'grayLight'
+					? colorMap[variantBgColor]
+					: colorMap['grayLight']
+			}
 		>
-			<Text fontFamily='heading' fontSize='10px' color='white'>
+			<Text
+				fontFamily='heading'
+				fontSize='10px'
+				color={variantBgColor === 'grayLight' ? 'gray.700' : 'white'}
+			>
 				{isNew ? 'NOVO' : 'USADO'}
 			</Text>
 		</View>
