51f9e0da6a3071b4b9b6e10338748c1e56115bc7
Welison Wessling <wwelisonw@gmail.com>
2024-02-14 19:09:09 -0300

    fix: ajusted nested navigation

--- mobile/src/routes/app/Stack.routes.tsx
+++ mobile/src/routes/app/Stack.routes.tsx
@@ -2,12 +2,18 @@ import {
 	createNativeStackNavigator,
 	NativeStackNavigationProp,
 } from '@react-navigation/native-stack';
+
+import { AppTabNavigationRoutesProps, TabRoutes } from './Tabs.routes';
+
 import { DetailsAdvertisement } from '@screens/app/DetailsAdvertisement/DetailsAdvertisement';
+import { CreateOrEditAdvertisement } from '@screens/app/CreateOrEditAdvertisement/CreateOrEditAdvertisement';
 
 type AppStackRoutes = {
+  homeTab: AppTabNavigationRoutesProps;
 	detailsadvertisement: {
 		id: string;
 	};
+	createoreditadvertisement: undefined;
 };
 
 export type AppStackNavigationRoutesProps =
@@ -18,7 +24,12 @@ const { Navigator, Screen } = createNativeStackNavigator<AppStackRoutes>();
 export const StackRoutes = () => {
 	return (
 		<Navigator screenOptions={{ animation: 'ios', headerShown: false }}>
+			<Screen name='homeTab' component={TabRoutes} />
 			<Screen name='detailsadvertisement' component={DetailsAdvertisement} />
+			<Screen
+				name='createoreditadvertisement'
+				component={CreateOrEditAdvertisement}
+			/>
 		</Navigator>
 	);
 };
