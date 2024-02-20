dc00b9e722376c3d06d18b9010cf6450f31a9a69
Welison Wessling <wwelisonw@gmail.com>
2024-02-20 16:32:09 -0300

    feat: created storageGetAuthToken

--- mobile/src/storage/authToken/authToken.ts
+++ mobile/src/storage/authToken/authToken.ts
@@ -4,3 +4,9 @@ import { AUTH_TOKEN_STORAGE } from '@storage/storageConfig';
 export const storageAuthTokenSave = async (token: string) => {
 	await AsyncStorage.setItem(AUTH_TOKEN_STORAGE, JSON.stringify(token));
 };
+
+export const storageGetAuthToken = async () => {
+	const token = await AsyncStorage.getItem(AUTH_TOKEN_STORAGE);
+
+	return token;
+};
